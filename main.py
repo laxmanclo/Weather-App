402cc947d67dc0a545f8d978cdb6b7a845984840
hax <103890297+haxthehacc@users.noreply.github.com>
2023-11-30 19:52:09 +0530

    Update main.py
    Changed main window style and date format to dd-mm-yyyy

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import tkinter as tk
 import requests
+from tkinter import messagebox
 from PIL import Image, ImageTk
+import ttkbootstrap as tkb
 import matplotlib.pyplot as plt
 from datetime import datetime
 import io
 
+
 def get_weather():
+    
     city = city_entry.get()  # Get the city from the entry field
-    if city:  # Check if a city name is provided
+    if city: # Check if a city name is provided
         # Make a GET request to the OpenWeatherMap API (replace 'your_api_key' with your actual API key)
-        response_current = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=6ae2f71d7704cfd1dbd8f08d54e3e15e&units=metric')
-        response_forecast = requests.get(f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid=6ae2f71d7704cfd1dbd8f08d54e3e15e&units=metric')
+        response_current = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=36e819fd6c1f509a31c217f689258890&units=metric')
+        response_forecast = requests.get(f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid=36e819fd6c1f509a31c217f689258890&units=metric')
         
         # Check if the API request was successful (status code 200)
         if response_current.status_code == 200 and response_forecast.status_code == 200:
@@ -21,14 +25,14 @@ def get_weather():
                 # Check if 'main' key exists in weather_current data
                 if 'main' in weather_current and 'weather' in weather_current:
                     # Update the labels with the weather data
-                    current_lbl['text'] = f"Current: {weather_current['main']['temp']}°C, {weather_current['weather'][0]['description']}"
+                    current_lbl['text'] = f"Current: {weather_current['main']['temp']}°C, {weather_current['weather'][0]['description']}" #type:ignore
 
                     # Display weather icons
                     current_icon_url = f"http://openweathermap.org/img/w/{weather_current['weather'][0]['icon']}.png"
                     load_current_icon(current_icon_url)
 
                     for i in range(5):
-                        forecast_lbls[i]['text'] = f"{datetime.fromtimestamp(weather_forecast['list'][i*8]['dt']).strftime('%Y-%m-%d')}: {weather_forecast['list'][i*8]['main']['temp']}°C, {weather_forecast['list'][i*8]['weather'][0]['description']}"
+                        forecast_lbls[i]['text'] = f"{datetime.fromtimestamp(weather_forecast['list'][i*8]['dt']).strftime('%d-%m-%Y')}: {weather_forecast['list'][i*8]['main']['temp']}°C, {weather_forecast['list'][i*8]['weather'][0]['description']}" #type:ignore
 
                         # Display weather icons for forecast
                         forecast_icon_url = f"http://openweathermap.org/img/w/{weather_forecast['list'][i*8]['weather'][0]['icon']}.png"
@@ -37,13 +41,28 @@ def get_weather():
                     # Create a simple temperature graph
                     create_temperature_graph(weather_forecast)
                 else:
-                    current_lbl['text'] = "Weather data not available"
+                    current_lbl['text'] = "Weather data not available" #type:ignore
             except KeyError as e:
-                current_lbl['text'] = f"Error: {e}"
+                current_lbl['text'] = f"Error: {e}" #type:ignore
         else:
-            current_lbl['text'] = "Failed to fetch weather data"
+            current_lbl['text'] = "Failed to fetch weather data" #type:ignore
     else:
-        current_lbl['text'] = "Please enter a city name"
+        current_lbl['text'] = "Please enter a city name" #type:ignore
+def search():
+    city=city_entry.get()
+    result=get_weather()
+    if result is None:
+        return
+    icon_url, temperature, description, city, country = result # type: ignore
+    location_label.configure(text=f"{city}, {country}")
+
+    image=Image.open(requests.get(icon_url, stream=True).raw)
+    icon=ImageTk.PhotoImage(image)
+    icon_label.configure(image=icon)
+    icon_label.image=icon # type: ignore
+
+    temperature_label.configure(text=f"Temperature: {temperature:.2f}°C")
+    description_label.configure(text=f"Description: {description}")
 
 def load_current_icon(url):
     response = requests.get(url, stream=True)
@@ -52,7 +71,7 @@ def load_current_icon(url):
     img = img.resize((50, 50))
     current_icon = ImageTk.PhotoImage(img)
     current_icon_label.configure(image=current_icon)
-    current_icon_label.image = current_icon
+    current_icon_label.image = current_icon # type: ignore
 
 def load_forecast_icon(url, index):
     response = requests.get(url, stream=True)
@@ -67,7 +86,7 @@ def create_temperature_graph(weather_forecast):
     dates = []
     temperatures = []
     for i in range(5):
-        dates.append(datetime.fromtimestamp(weather_forecast['list'][i*8]['dt']).strftime('%Y-%m-%d'))
+        dates.append(datetime.fromtimestamp(weather_forecast['list'][i*8]['dt']).strftime('%d-%m-%Y'))
         temperatures.append(weather_forecast['list'][i*8]['main']['temp'])
 
     plt.figure(figsize=(8, 4))
@@ -85,24 +104,34 @@ def save_preferences():
     city = city_entry.get()
     with open('user_preferences.txt', 'w') as file:
         file.write(city)
-    window.destroy()  # Close the window upon saving preferences
+    window.destroy() # Close the window upon saving preferences
 
 # Create the main window
-window = tk.Tk()
+window=tkb.Window(themename="morph")
 window.title("Weather App")
-
-# Set background image
-background_image = Image.open("pexels-johannes-plenio-1118873.jpg")  # Replace with your image path
-background_image = background_image.resize((800, 600), Image.BILINEAR)  # Example: Using BILINEAR resampling filter
-background_photo = ImageTk.PhotoImage(background_image)
-background_label = tk.Label(window, image=background_photo)
-background_label.place(x=0, y=0, relwidth=1, relheight=1)
+window.geometry("1280x720")
 
 # Create the entry field and search button
-city_entry = tk.Entry(window, font=("Helvetica", 12))
-city_entry.pack(padx=10, pady=5)
-search_button = tk.Button(window, text="Search Weather", command=get_weather, font=("Helvetica", 12))
-search_button.pack(pady=5)
+city_entry=tkb.Entry(window, font="Helvetica, 18")
+city_entry.pack(pady=10)
+search_button=tkb.Button(window, text="Search", command=search, bootstyle="warning") # type: ignore
+search_button.pack(pady=10)
+
+# Create the label for location
+location_label=tk.Label(window, font="Helvetica, 25")
+location_label.pack(pady=20)
+
+# Create the label for weather icons
+icon_label=tk.Label(window)
+icon_label.pack()
+
+# Create the label for temperature
+temperature_label=tk.Label(window, font="Helvetica, 20")
+temperature_label.pack()
+
+# Create the label for weather description
+description_label=tk.Label(window, font="Helvetica, 20")
+description_label.pack()
 
 # Create the label for the current weather
 current_lbl = tk.Label(window, text="", font=("Helvetica", 16, "bold"))
